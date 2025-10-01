10a75a35f1894e3999dcd533bcd0e28ff9f4acab
GitVladislavHub <vladislav.drake31@gmail.com>
2025-10-01 18:17:16 +0300

    Задание_1_Испр_без_загрузки

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
-import random
-import string
+from faker import Faker
 
 import pytest
 from selenium import webdriver
-
+fake = Faker()
 
 @pytest.fixture()
 def browser():
@@ -14,12 +13,9 @@ def browser():
 
 @pytest.fixture()
 def random_email():
-    name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(8))
-    domain = ["gmail.com", "mail.ru", "icloud.com"]
-    return f"{name}@{random.choice(domain)}"
+    return fake.email()
 
 
 @pytest.fixture()
-def random_password(length=9):
-    chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for _ in range(length))
+def random_password():
+    return fake.password(length=8)
--- tests/test_1.py
+++ tests/test_1.py
@@ -6,38 +6,39 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 STEAM_GLOBAL_LINK = "https://store.steampowered.com/"
 EMAIL_INPUT_KEYBOARD = By.XPATH, '(//input[@type="text"])[2]'  # исправить
-PASSWORD_INPUT_KEYBOARD = By.XPATH, '//input[contains(@class, "_2GBWeup5cttgbTw8FM3tfx") and contains(@type, "password")]'
+PASSWORD_INPUT_KEYBOARD = By.XPATH, '//input[contains(@type, "password")]'
 LOGIN_LINK = By.XPATH, "//a[contains(@class, 'global_action_link')]"
 TIMEOUT = 12
-BUTTON_LOGIN = '//button[contains(@class, "DjSvCZoKKfoNSmarsEcTS")]'
+BUTTON_LOGIN = '//button[contains(@type, "submit") and contains(text(), "Войти")]'
+CAROUSEL_ITEMS = By.XPATH, "//div[@aria-label='ИЗБРАННЫЕ ПРЕДЛОЖЕНИЯ']"
+CREATE_ACCOUNT = By.XPATH, '//a[contains(@class, "login_create_btn btn_blue_steamui btn_medium")]'
+TEXT_WARNING = By.XPATH, "//div[contains(text(), 'проверьте свой пароль и имя аккаунта')]"
 
 
 def test_login(browser, random_email, random_password):
     browser.get(STEAM_GLOBAL_LINK)
+    home_page = WebDriverWait(browser, TIMEOUT).until(EC.visibility_of_element_located(CAROUSEL_ITEMS))
+
+    assert home_page.is_displayed(), "Главная страница еще не загрузилась"
 
     click_button = WebDriverWait(browser, TIMEOUT).until(EC.element_to_be_clickable(LOGIN_LINK))
     click_button.click()
 
+    assert "/login" in browser.current_url, "Ошибка: не та страница!"
+
     input_email = WebDriverWait(browser, TIMEOUT).until(EC.element_to_be_clickable(EMAIL_INPUT_KEYBOARD))
     input_email.send_keys(random_email)
-
-    actual_email = input_email.get_attribute("value")
-    expected_email = random_email
-
-    assert actual_email == expected_email, (
-        f"Expected:'{expected_email}', Actual: '{actual_email}'"
-    )
 
     input_password = WebDriverWait(browser, TIMEOUT).until(EC.element_to_be_clickable(PASSWORD_INPUT_KEYBOARD))
     input_password.send_keys(random_password)
 
-    actual_password = input_password.get_attribute("value")
-    expected_password = random_password
-
-    assert actual_password == expected_password, (
-        f"Expected:'{expected_password}', Actual: '{actual_password}'"
-    )
-
     button_login = browser.find_element(By.XPATH, BUTTON_LOGIN)
-
     button_login.click()
+
+
+
+    text_error = WebDriverWait(browser, TIMEOUT).until(EC.visibility_of_element_located(TEXT_WARNING))
+
+    assert text_error.is_displayed(), "Текст предупреждения не появился"
+
+    time.sleep(2)
