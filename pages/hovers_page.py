be693e34e5d8be30e46b4c24bf991c0e161f8713
GitVladislavHub <vladislav.drake31@gmail.com>
2025-10-30 18:13:50 +0300

    Part_9_v3

--- pages/hovers_page.py
+++ pages/hovers_page.py
@@ -12,8 +12,6 @@ class HoversPage(BasePage):
     HOVER_HREF_LOC = "(//a[contains(text(), 'View profile')])[{index}]"
     TEXT_HOVER_LOC = "(//h5[contains(text(), 'name: user')])[{index}]"
 
-    USER_PAGE_TEXT_LOC = "//h2[contains(text(), 'Sinatra')]"
-
     def __init__(self, browser):
         super().__init__(browser)
         self.page_name = "Hovers_page_1"
@@ -23,7 +21,6 @@ class HoversPage(BasePage):
         self.hover_element_href = Button(browser, self.HOVER_HREF_LOC,
                                          description="Hover_element_1 -> User_profile_page")
 
-        self.user_element_loc = Label(browser, self.USER_PAGE_TEXT_LOC, description="User_loc -> None")
         self.unique_element = self.hover_element_image_uniq
 
     def get_text_hover_element(self, browser, user_index):
@@ -33,19 +30,14 @@ class HoversPage(BasePage):
         hover_text_locator = self.TEXT_HOVER_LOC.format(index=user_index)
         text_hover_element = Label(browser, hover_text_locator, description="User_loc -> None")
 
-        href_locator = self.HOVER_HREF_LOC.format(index=user_index)
-        hover_href_element = Button(browser, href_locator, description="Hover_href_1 -> None")
-
         image_el = image_element.wait_for_visible()
         action_chains = ActionChains(browser.driver)
         action_chains.move_to_element(image_el).perform()
 
         text = text_hover_element.get_text()
-        hover_href_element.click()
         return text
 
-    def go_on_hovers_page(self):
-        self.browser.go_back()
-        self.wait_for_open()
-        element_page = self.hover_element_image_uniq.wait_for_visible()
-        return element_page
+    def click_view_profile(self, browser, user_index):
+        href_locator = self.HOVER_HREF_LOC.format(index=user_index)
+        hover_href_element = Button(browser, href_locator, description="Hover_href_1 -> None")
+        hover_href_element.click()
--- tests/test_6.py
+++ tests/test_6.py
@@ -1,5 +1,8 @@
+import time
+
 from config_reader import ConfigReader
 from pages.hovers_page import HoversPage
+from pages.view_profile_page import ViewProfilePage
 
 config = ConfigReader()
 
@@ -9,9 +12,57 @@ def test_hover(browser):
     page_hover.browser.get(config.base_urls["6_hovers"])
     page_hover.wait_for_open()
 
+    page_hover_main = page_hover.browser.get_current_window_handle()
+
     actual = page_hover.get_text_hover_element(browser, 1)
     expected = "name: user1"
     assert actual == expected, f"Expected: {expected}, "f"Actual: {actual}"
 
-    actual = page_hover.go_on_hovers_page()
-    assert actual, f"Expected: Страница успешно открыта! , "f"Actual: {actual}"
+    page_hover.click_view_profile(browser, user_index=1)
+
+    view_profile_page = ViewProfilePage(browser)
+    view_profile_page.wait_for_open()
+
+    view_profile_page_main = view_profile_page.browser.get_current_window_handle()
+
+    actual = view_profile_page.get_text_view_profile()
+    expected = "Sinatra doesn’t know this ditty."
+    assert actual == expected
+
+    browser.go_back()
+
+    page_hover.wait_for_open()
+
+    actual = page_hover.get_text_hover_element(browser, 2)
+    expected = "name: user2"
+    assert actual == expected, f"Expected: {expected}, "f"Actual: {actual}"
+
+    page_hover.click_view_profile(browser, user_index=2)
+
+    view_profile_page = ViewProfilePage(browser)
+    view_profile_page.wait_for_open()
+
+    actual = view_profile_page.get_text_view_profile()
+    expected = "Sinatra doesn’t know this ditty."
+    assert actual == expected
+
+    browser.go_back()
+
+    page_hover.wait_for_open()
+
+    actual = page_hover.get_text_hover_element(browser, 3)
+    expected = "name: user3"
+    assert actual == expected, f"Expected: {expected}, "f"Actual: {actual}"
+
+    page_hover.click_view_profile(browser, user_index=3)
+
+    view_profile_page = ViewProfilePage(browser)
+    view_profile_page.wait_for_open()
+
+    actual = view_profile_page.get_text_view_profile()
+    expected = "Sinatra doesn’t know this ditty."
+    assert actual == expected
+
+    browser.go_back()
+
+    page_hover.wait_for_open()
