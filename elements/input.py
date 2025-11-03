1c5bc23fbe6b84f614103e291dcf18361c18cf7a
GitVladislavHub <vladislav.drake31@gmail.com>
2025-11-03 15:40:06 +0300

    Part_11_v1

--- elements/input.py
+++ elements/input.py
@@ -38,3 +38,11 @@ class Input(BaseElement):
         element = self.wait_for_presence()
         Logger.info(f"{self}: js_send_keys = '{keys}'")
         self.browser.execute_script("arguments[0].value = arguments[1]", element, keys)
+
+    def send_keys_upload_file(self, keys):
+        element = self.wait_for_clickable()
+        if element.get_attribute("type") == "file":
+            element.send_keys(keys)
+        else:
+            self.clear()
+            element.send_keys(keys)
---
+++ tests/test_11.py
@@ -0,0 +1,14 @@
+import time
+
+from config_reader import ConfigReader
+from pages.upload_image_page import UploadImagePage
+
+config = ConfigReader()
+
+
+def test_upload_image(browser):
+    upload_image = UploadImagePage(browser)
+    upload_image.browser.get(config.base_urls["11_upload"])
+    upload_image.wait_for_open()
+
+    upload_image.upload_image()
